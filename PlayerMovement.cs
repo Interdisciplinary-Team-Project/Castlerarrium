4676d3e6b7eaa9b7fa4f27fee8d588d018c93448
x17399691 <56955523+x17399691@users.noreply.github.com>
2020-04-20 20:07:38 +0100

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+    public CharacterController2D controller;
+    public Animator animator; 
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    
+    
+    void Start()
+    {
+        
+    }
+ 
+    void Update()
+    
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;  
+            animator.SetBool("IsJumping", true);
+		}
+        
+    }
+
+    public void OnLanding ()
+    {
+        animator.SetBool("IsJumping", false);
+	}
+
+    void FixedUpdate ()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+        }
+
+
+    void OnCollisionEnter2D (Collision2D col)
+    {
+     if (col.gameObject.tag.Equals ("Enemy"))
+     {
+      gameObject.SetActive (false);
+	 }
+	}
+}
+
---
+++ enemy_move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_move : MonoBehaviour
+{
+    public float speed;
+
+    public bool MoveRight;
+    
+    void Update()
+    {
+        if(MoveRight){
+          transform.Translate(2 * Time.deltaTime * speed , 0,0);
+		}
+        else{
+          transform.Translate(-2 * Time.deltaTime * speed , 0,0);  
+		}
+        }
+    void OnTriggerEnter2D(Collider2D trig)
+        {
+            if(trig.gameObject.CompareTag("turn")){
+            
+            if(MoveRight){
+               MoveRight = false;
+			}
+            else{
+               MoveRight = true;     
+			}
+			
+	    }
+    }
+}
